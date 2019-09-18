7725ba7bab04e69abacf09d2804f0eca518d3f77
BladLust <timty.tsui@gmail.com>
2019-09-18 11:30:29 +0800

    new c++ playground for syntax testing

---
+++ VSCodeWorkspace/cpptry/default.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    cout << 17 % 5 << "\t" << (-17) % 5 << "\t" << -17 % 5 << "\t" << 17 % (-5) << "\t" << (-17) % (-5) << endl;
+    return 0;
+}
